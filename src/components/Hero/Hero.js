0508d19530ea0dc5d9bb8513a18f6558ae98b866
Ana Ausek <anac_ausek@hotmail.com>
2021-11-09 23:21:41 -0500

    New develop branch and home page components

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,20 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row padding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of this portfolio page is to showcase some of my professional 
+        and academic achievements in the are of software development.
+      </SectionText>
+      <Button onClick={() => window.open("https://www.linkedin.com/in/ana-ausek/", "_blank")}>More About Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          {/* <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/> */}
+          {/* <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"/>  */}
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet"/> 
         </Head>
         <body>
           <Main />
