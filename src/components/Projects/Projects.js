6157b1bedbcd4febdcfd4375aa3d40b09b6d00e6
Ana Ausek <anac_ausek@hotmail.com>
2021-11-09 23:39:06 -0500

    Deploying to GH pages

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,45 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+// const projectsData = [
+//   {
+//     title: "Project 1",
+//     description: "This is project 1",
+//   },
+//   {
+//     title: "Project 2",
+//     description: "This is project 2",
+//   },
+//   {
+//     title: "Project 3",
+//     description: "This is project 3",
+//   },
+//   {
+//     title: "Project 4",
+//     description: "This is project 4",
+//   },
+//   {
+//     title: "Project 5",
+//     description: "This is project 5",
+//   },
+// ];
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section>
+    <SectionDivider>
+    </SectionDivider>
+    <SectionTitle></SectionTitle>
+    <GridContainer>
+      {/* Import from constants/constants */}
+      {projects.map((project) => (
+        <div>
+          {project.title}
+          <br/>
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
