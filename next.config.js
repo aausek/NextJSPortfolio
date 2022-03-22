f7fe140b9583dabfbc58e98bca20fcb20c90a488
Ana Ausek <anac_ausek@hotmail.com>
2022-03-22 10:58:30 -0400

    Removing token in config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,5 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/NextJSPortfolio/" : "",
-  env: {
-    NEXT_PUBLIC_TOKEN: 'eyJhcGlfa2V5IjoiSXdOZmJZR1JDUVk5TGhBRHl0a3dYbXEzV2RRVHA5MnQ2ZTJQZlpRekRscz0ifQ==',
-  }
+  assetPrefix: isProd ? "/NextJSPortfolio/" : ""
 };
